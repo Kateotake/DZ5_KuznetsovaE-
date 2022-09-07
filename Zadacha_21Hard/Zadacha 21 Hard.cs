0913d9f2bdcb12484dfc2cb28e800c855d53e76e
KuznetsovaE <kettt56@mail.ru>
2022-09-08 01:29:41 +0300

    2

--- Zadacha_21Hard/Zadacha 21 Hard.cs
+++ Zadacha_21Hard/Zadacha 21 Hard.cs
@@ -2,44 +2,6 @@
 // Напишите программу, которая принимает на вход координаты точек и находит расстояние между ними в N-мерном пространстве. 
 //N - задается пользователем.
 
-// void PrintArray(int[] arr) 
-// {
-//     for (int i = 0; i < arr.Length; i++)
-//     {
-//         Console.Write($"{arr[i]} ");
-//     }
-//     Console.WriteLine();
-// }
-
-// Console.WriteLine("Введите число N");
-// int N = Convert.ToInt32(Console.ReadLine());
-// int [] array_x = new int[N];
-// int [] array_y = new int[N];
-// Console.WriteLine ("Введите координаты точки Х:");
-// for (int i = 0; i < N; i++)
-// {
-//     array_x[i] = Convert.ToInt32(Console.ReadLine());
-// }
-// Console.WriteLine ("Введите координаты точки Y:");
-// for (int j = 0; j < N; j++)
-// {
-//     array_y[j] = Convert.ToInt32(Console.ReadLine());
-// }
-// Console.Write("Координаты точки Х : ");
-// PrintArray(array_x);
-// Console.Write("Координаты точки Y : ");
-// PrintArray(array_y);
-
-// double sum = 0;
-// for (int i = 0; i < N; i++)
-// {
-//     sum += (array_x[i] - array_y[i]) * (array_x[i] - array_y[i]);
-// }
-
-// double res = Math.Sqrt(sum);
-// Console.WriteLine ($"Расстояние между точками X и Y равно {res} ");
-
-
 
 void PrintArray(int[] arr) 
 {
